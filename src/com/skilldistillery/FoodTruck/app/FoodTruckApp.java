7a846d3ce1fa3cfe97574cf8093ccab4af097690
Shabir Nawabi <Shabir.n@gmail.com>
2023-03-24 16:35:59 -0700

    initial foodtruck bits

---
+++ src/com/skilldistillery/FoodTruck/app/FoodTruckApp.java
@@ -0,0 +1,19 @@
+package com.skilldistillery.FoodTruck.app;
+
+import com.skilldistillery.FoodTruck.entities.FoodTruck;
+
+public class FoodTruckApp {
+	private FoodTruck[] fleet;
+
+	public static void main(String[] args) {
+		
+		FoodTruckApp fta = new FoodTruckApp();
+		fta.run();
+
+	}
+	
+	public void run() {
+		
+	}
+
+}
---
+++ src/com/skilldistillery/FoodTruck/entities/FoodTruck.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.FoodTruck.entities;
+
+public class FoodTruck {
+	private String Name;
+	
+	//ctors
+	
+	
+	//getters and settrs
+	
+	//toString
+}
